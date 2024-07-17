f1076923e74778466a0e7a77c5ce3dabf6ccf98c
LDLaura <despesselaura@yahoo.fr>
2024-07-17 15:02:32 +0200

    feat(gender): update

--- src/Controller/Backend/GenderController.php
+++ src/Controller/Backend/GenderController.php
@@ -17,17 +17,18 @@ class GenderController extends AbstractController
 {
     public function __construct(
         private EntityManagerInterface $em,
-    ){  
+    ) {
     }
 
-    #[Route('', name : '.index', methods:['GET', 'POST'])]
-    public function index(GenderRepository $repo): Response{
+    #[Route('', name: '.index', methods: ['GET', 'POST'])]
+    public function index(GenderRepository $repo): Response
+    {
         return $this->render('Backend/Gender/index.html.twig', [
             'gender' => $repo->findAll(),
         ]);
     }
 
-    #[Route('/create', name: '.create', methods:['GET', 'POST'])]
+    #[Route('/create', name: '.create', methods: ['GET', 'POST'])]
     public function create(Request $request): Response
     {
         $gender = new Gender();
@@ -45,6 +46,32 @@ class GenderController extends AbstractController
         }
 
         return $this->render('Backend/Gender/create.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}/update', name: '.update', methods: ['GET', 'POST'])]
+    public function update(?Gender $gender, Request $request): Response
+    {
+        if (!$gender) {
+            $this->addFlash('error', 'Le genre n\'existe pas');
+
+            return $this->redirectToRoute('admin.gender.index');
+        }
+
+        $form = $this->createForm(GenderType::class, $gender);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->em->persist($gender);
+            $this->em->flush();
+
+            $this->addFlash('success', 'Le genre a bien été modifié');
+
+            return $this->redirectToRoute('admin.gender.index');
+        }
+        
+        return $this->render('Backend/Gender/update.html.twig', [
             'form' => $form,
         ]);
     }
